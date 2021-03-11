1569fb9afec7e31e70d5d7c90102b6e699429be6
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-03-11 18:52:13 +0800

    beta-reduction

--- core.rkt
+++ core.rkt
@@ -9,9 +9,9 @@
   (Expr (e)
         id
         (cons e* ...)
-        (lambda tele* ... e)
-        (pi tele* ... e)
-        (sigma tele* ... e)
+        (lambda (tele* ...) e)
+        (pi (tele* ...) e)
+        (sigma (tele* ...) e)
         (app e e* ...)
         (type level))
   (Telescope (tele)
@@ -20,7 +20,64 @@
   (Type (ty) e)
   (Level (level) num))
 
+(define-pass subst : Core (e m) -> Core ()
+  (S : Expr (e) -> Expr ()
+     [,id (if (hash-ref m id #f)
+              (hash-ref m id)
+              id)]
+     [(cons ,[e*] ...)
+      `(cons ,e* ...)]
+     [(lambda (,tele* ...) ,e)
+      (define m2 (hash-copy m))
+      (for ([tele tele*])
+        (nanopass-case
+         (Core Telescope) tele
+         [(explicit ,id ,ty)
+          (hash-remove! m2 id)]
+         [(implicit ,id ,ty)
+          (hash-remove! m2 id)]))
+      `(lambda (,tele* ...) ,[subst e m2])]
+     [(pi (,tele* ...) ,[e])
+      `(pi (,tele* ...) ,e)]
+     [(sigma (,tele* ...) ,[e])
+      `(sigma (,tele* ...) ,e)]
+     [(app ,[e] ,[e*] ...)
+      `(app ,e ,e* ...)])
+  (S e))
+
+(define-pass β-reduce : Core (e) -> Core ()
+  (R : Expr (e) -> Expr ()
+     [(app (lambda (,tele* ...) ,e) ,e* ...)
+      (let ([tele*-len (length tele*)]
+            [e*-len (length e*)])
+        (unless (= tele*-len e*-len)
+          (error 'arity "need ~a, get ~a" tele*-len e*-len)))
+      (define m (make-hash))
+      (for ([tele tele*]
+            [e e*])
+        (nanopass-case
+         (Core Telescope) tele
+         [(explicit ,id ,ty)
+          (hash-set! m id e)]
+         [(implicit ,id ,ty)
+          (hash-set! m id e)]))
+      (subst e m)])
+  (R e))
+
 (define-parser parse-core Core)
 
 (module+ test
-  (parse-core '(cons n m)))
+  (unparse-Core (parse-core '(cons n m)))
+
+  (unparse-Core
+   (β-reduce (parse-core
+              '(app (lambda ([explicit x A] [explicit y A])
+                      (cons x y))
+                    a b))))
+  ; should escape
+  (unparse-Core
+   (β-reduce (parse-core
+              '(app (lambda ([explicit x A])
+                      (lambda ([explicit x A])
+                        x))
+                    a)))))
