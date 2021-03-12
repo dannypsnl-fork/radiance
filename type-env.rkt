21fc9fbcda0d487821014b6fd44439f6db9268e3
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-03-12 18:05:28 +0800

    type of

---
+++ type-env.rkt
@@ -0,0 +1,42 @@
+#lang typed/racket
+
+(require "term.rkt")
+
+(struct TypeEnv
+  ([parent : (Option TypeEnv)]
+   [binds : (Immutable-HashTable Symbol Type)])
+  #:transparent)
+
+(: make-TypeEnv (->* [(Listof (Pair Symbol Type))]
+                     [TypeEnv]
+                     ;--------
+                     TypeEnv))
+(define (make-TypeEnv l [parent #f])
+  (TypeEnv parent (make-immutable-hash l)))
+
+(: typeof : TypeEnv Term -> Type)
+(define (typeof tyenv term)
+  (match term
+    [(Lambda tele+ body)
+     (define param-typs
+       (map (λ ([tele : Telescope])
+              (cons (Telescope-var tele) (Telescope-typ tele)))
+            tele+))
+     (define new-env (make-TypeEnv param-typs tyenv))
+     (Pi tele+ (typeof new-env body))]
+    [id #:when (symbol? term)
+        (define r? (hash-ref (TypeEnv-binds tyenv) id #f))
+        (unless r?
+          (error 'not-found "~a" id))
+        r?]))
+
+(module+ test
+  (require typed/rackunit)
+
+  (define env (make-TypeEnv '((zero . Nat))))
+  (check-equal? (typeof env 'zero)
+                'Nat)
+
+  (check-equal? (typeof env (Lambda (list (Explicit 'x 'Nat))
+                                    'x))
+                (Pi (list (Explicit 'x 'Nat)) 'Nat)))
